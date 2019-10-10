db2ee77ae79ccfec5a7b59c700dc880e04a6948c
vuongsoi <vuongsoi94@gmail.com>
2019-10-10 18:15:42 +0700

    123

---
+++ bai1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a;
+	scanf("%d",&a);
+	if(a>0){
+		printf("Day la so duong\n");
+}else{
+printf("Day la so am\n");
+}
+	
+	return 0;
+}
---
+++ bai2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	scanf("%d%d",&a,&b);
+	if(a>b){
+		printf("day la so lon nhat\n%d",a);
+}else{
+	printf("day la so lon nhat\n%d",b);
+	}
+	
+	return 0;
+}
